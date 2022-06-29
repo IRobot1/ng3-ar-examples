5d882b26e0fdbc80bfdc66d66ec8a53f69ef8b12
Adrian Moore <adrianrob@hotmail.com>
2022-06-29 10:28:55 -0700

    Added NGT and NG3 WebXR support and home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { NgtCanvasModule } from '@angular-three/core';
+
+import { NgtColorAttributeModule } from '@angular-three/core/attributes';
+import { NgtGridHelperModule } from '@angular-three/core/helpers';
+
+import { NgtPointLightModule } from '@angular-three/core/lights';
+
+import { NgtSobaOrbitControlsModule } from '@angular-three/soba/controls';
+
+import { Ng3WebxrModule } from 'ng3-webxr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    NgtCanvasModule,
+    NgtColorAttributeModule,
+
+    NgtGridHelperModule,
+
+    NgtPointLightModule,
+
+    NgtSobaOrbitControlsModule,
+
+    Ng3WebxrModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
